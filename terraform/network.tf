682c4d7c255474bfa28ebd3a0c8d8bc41a4f949f
Bee0933 <bestnyah7@gmail.com>
2024-11-26 16:37:36 +0100

    wip: add prometheus & grafana configs
    Signed-off-by: Bee0933 <bestnyah7@gmail.com>

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,4 +1,4 @@
-# source server firewall configs 
+# Airflow server firewall configs 
 resource "digitalocean_firewall" "airflow-server-fw" {
   name = "airflow-server-firewall"
 
@@ -67,6 +67,108 @@ resource "digitalocean_firewall" "airflow-server-fw" {
   outbound_rule {
     protocol              = "tcp"
     port_range            = "9100"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+
+# Monitor server firewall configs 
+resource "digitalocean_firewall" "monitor-server-fw" {
+  name = "monitor-server-firewall"
+
+  droplet_ids = [digitalocean_droplet.monitor-server-0.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # node exporter
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9100"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # prometheus
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9090"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # grafana
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "3000"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "80"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "443"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # node exporter
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "9100"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # prometheus
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "9090"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # grafana
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "3000"
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
